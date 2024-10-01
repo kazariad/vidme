e233fdd85a77baad71f53320eab7e5552115d9ab
David K <122331465+kazariad@users.noreply.github.com>
2024-10-01 15:54:05 -0700

    Init

---
+++ src/main/java/dev/dkaz/vidme/VidmeApplication.java
@@ -0,0 +1,13 @@
+package dev.dkaz.vidme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VidmeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/dkaz/vidme/VidmeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dkaz.vidme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidmeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
