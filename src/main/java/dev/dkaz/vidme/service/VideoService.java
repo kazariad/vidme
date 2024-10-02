a7318b78e3f8c851e59b30c9317ec7099f560f19
David K <122331465+kazariad@users.noreply.github.com>
2024-10-02 02:33:20 -0700

    Add VideoService

---
+++ src/main/java/dev/dkaz/vidme/service/VideoService.java
@@ -0,0 +1,28 @@
+package dev.dkaz.vidme.service;
+
+import dev.dkaz.vidme.model.Video;
+import dev.dkaz.vidme.persistence.VideoFileStorage;
+import dev.dkaz.vidme.persistence.VideoRepository;
+import org.springframework.stereotype.Service;
+
+import java.nio.file.Path;
+import java.util.Optional;
+
+@Service
+public class VideoService {
+    private final VideoRepository videoRepository;
+    private final VideoFileStorage videoFileStorage;
+
+    public VideoService(VideoRepository videoRepository, VideoFileStorage videoFileStorage) {
+        this.videoRepository = videoRepository;
+        this.videoFileStorage = videoFileStorage;
+    }
+
+    public Optional<Video> findVideoById(Long id) {
+        return videoRepository.findById(id);
+    }
+
+    public Optional<Path> findVideoFileByFileId(String fileId) {
+        return videoFileStorage.findByFileId(fileId);
+    }
+}
