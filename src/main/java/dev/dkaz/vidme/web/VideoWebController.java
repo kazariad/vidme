1df381647131da031f2c4d20f13bec50cf6a740c
David K <122331465+kazariad@users.noreply.github.com>
2024-10-02 14:39:50 -0700

    Add /video page template

--- src/main/java/dev/dkaz/vidme/web/VideoWebController.java
+++ src/main/java/dev/dkaz/vidme/web/VideoWebController.java
@@ -1,25 +1,42 @@
 package dev.dkaz.vidme.web;
 
+import dev.dkaz.vidme.model.Video;
+import dev.dkaz.vidme.service.TimeUtils;
 import dev.dkaz.vidme.service.VideoService;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.nio.file.Path;
+import java.time.Instant;
 import java.util.Optional;
 
 @Controller
 public class VideoWebController {
     private final VideoService videoService;
+    private final TimeUtils timeUtils;
     private final MediaType mediaType = new MediaType("video", "webm");
 
-    public VideoWebController(VideoService videoService) {
+    public VideoWebController(VideoService videoService, TimeUtils timeUtils) {
         this.videoService = videoService;
+        this.timeUtils = timeUtils;
+    }
+
+    @GetMapping(path = "/video/{id}")
+    public String getVideo(@PathVariable Long id, Model model) {
+        Optional<Video> videoOpt = videoService.findVideoById(id);
+        if (videoOpt.isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        Video video = videoOpt.get();
+        model.addAttribute("video", video);
+        // use relative time to avoid determining client's time zone
+        model.addAttribute("timeSinceUpload", timeUtils.getElapsedTime(video.getCreatedAt(), Instant.now()));
+        return "video";
     }
 
     @GetMapping(path = "/video/stream/{fileId}")
