4098853fee0777f9bb3c24b40462fee0d42e6344
David K <122331465+kazariad@users.noreply.github.com>
2024-10-02 02:32:44 -0700

    Add VideoFileStorage

---
+++ src/main/java/dev/dkaz/vidme/persistence/VideoFileStorage.java
@@ -0,0 +1,42 @@
+package dev.dkaz.vidme.persistence;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+@Component
+public class VideoFileStorage {
+    private final Path ROOT;
+    private final Map<String, Path> LOOKUP_CACHE;
+
+    public VideoFileStorage(@Value("${vidme.video.root-dir}") String rootDir) {
+        ROOT = Paths.get(rootDir);
+        LOOKUP_CACHE = new ConcurrentHashMap<>();
+    }
+
+    public Optional<Path> findByFileId(String fileId) {
+        if (LOOKUP_CACHE.containsKey(fileId)) {
+            return Optional.of(LOOKUP_CACHE.get(fileId));
+        }
+
+        try (Stream<Path> paths = Files.find(ROOT, 1, (path, basicFileAttributes) ->
+                path.getFileName().toString().equals(fileId + ".webm")
+        )) {
+            Optional<Path> pathOpt = paths.findFirst();
+            pathOpt.ifPresent(path -> LOOKUP_CACHE.put(fileId, path));
+            return pathOpt;
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+}
